7beb5f5c0ea6ad1445abca3f914f9743af12094f
Nicholas Kwon <nichkwon@gmail.com>
2015-09-26 16:03:19 -0500

    created the /group/create endpoint

--- amplify_api/models.py
+++ amplify_api/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from uuid import uuid4
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.conf import settings
-# Create your models here.
 
 class Group(models.Model):
     created = models.DateTimeField(auto_now_add=True)
