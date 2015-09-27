b0aa1f37a19925fbd6c66784631f3f649a7fb6e0
Nicholas Kwon <nichkwon@gmail.com>
2015-09-27 00:47:13 -0500

    removed migrations from .gitignore

---
+++ amplify_api/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('name', models.CharField(default=b'', unique=True, max_length=100, blank=True)),
+                ('song', models.CharField(default=b'', max_length=100, blank=True)),
+                ('master_start', models.DateTimeField(null=True, blank=True)),
+                ('slave_start', models.DateTimeField(null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('oauth', models.CharField(default=b'', max_length=100, serialize=False, primary_key=True, blank=True)),
+                ('is_master', models.BooleanField(default=False)),
+                ('group', models.ForeignKey(to='amplify_api.Group', null=True)),
+            ],
+            options={
+                'ordering': ('created',),
+            },
+        ),
+    ]
